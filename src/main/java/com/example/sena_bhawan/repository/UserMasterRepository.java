5127253d2bff35acccc0843004ab6d58b65fce8c
harsh <harsh.goyal@brancosoft.co.in>
2026-01-21 18:13:35 +0530

    Complete Course Eligibility Form

--- src/main/java/com/example/sena_bhawan/repository/UserMasterRepository.java
+++ src/main/java/com/example/sena_bhawan/repository/UserMasterRepository.java
@@ -5,8 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserMasterRepository
-        extends JpaRepository<UserMaster, Long> {
+public interface UserMasterRepository extends JpaRepository<UserMaster, Long> {
     Optional<UserMaster> findByUsername(String username);
 }
-
--- src/main/java/com/example/sena_bhawan/service/AdminService.java
+++ src/main/java/com/example/sena_bhawan/service/AdminService.java
@@ -1,20 +1,8 @@
 package com.example.sena_bhawan.service;
 
-import com.example.sena_bhawan.repository.UserMasterRepository;
-import org.springframework.stereotype.Service;
+import com.example.sena_bhawan.entity.UserMaster;
 
-@Service
-public class AdminService {
-
-    private final UserMasterRepository repository;
-
-    public AdminService(UserMasterRepository repository) {
-        this.repository = repository;
-    }
-
-    public boolean authenticate(String username, String password) {
-        return repository.findByUsername(username)
-                .map(UserMaster -> UserMaster.getPassword().equals(password))
-                .orElse(false);
-    }
+public interface AdminService {
+    UserMaster getUserByUsername(String username);
+    boolean authenticate(String username, String password);
 }
